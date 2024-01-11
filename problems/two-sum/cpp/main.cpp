a8e2fc8efaaad5fbe8cbbed385fea10d9f18e403
Marc Fong <marcjohnfong@gmail.com>
2024-01-11 23:30:44 +0800

    Update two-sum c++ solution

--- problems/two-sum/cpp/main.cpp
+++ problems/two-sum/cpp/main.cpp
@@ -6,8 +6,8 @@ public:
   std::vector<int> twoSum(std::vector<int> &nums, int target) {
     std::unordered_map<int, int> deltas(nums.size());
     for (auto i = 0; i < nums.size(); ++i) {
-      auto delta = target - nums[i];
-      if (auto found = deltas.find(delta); found != deltas.end()) {
+      const auto delta = target - nums[i];
+      if (const auto found = deltas.find(delta); found != deltas.end()) {
         return std::vector<int>{found->second, i};
       }
       deltas.insert({nums[i], i});
