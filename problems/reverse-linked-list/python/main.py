ee5b61f54baf200d87b38e52e1cdc659dc042d33
Marc Fong <marcjohnfong@gmail.com>
2024-01-21 21:27:17 +0800

    Update python type annotation for reverse-linked-list

--- problems/reverse-linked-list/python/main.py
+++ problems/reverse-linked-list/python/main.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 class ListNode:
     def __init__(self, val=0, next=None):
-        self.val: int = val
+        self.val = val
         self.next: Optional[ListNode] = next
 
 
