40936f133b0552dd92b2a921a0dfc92a7ee49cd1
Marc Fong <marcjohnfong@gmail.com>
2024-03-03 21:27:25 +0800

    Update solutions for remove-nth-node-from-end-of-list

---
+++ problems/remove-nth-node-from-end-of-list/go/main.go
@@ -0,0 +1,35 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	size := getListSize(head)
+	m := size - n
+	if m == 0 {
+		return head.Next
+	}
+	node := head
+	for range m - 1 {
+		node = node.Next
+	}
+	if node != nil && node.Next != nil {
+		node.Next = node.Next.Next
+	}
+	return head
+}
+
+func getListSize(head *ListNode) int {
+	size := 0
+	node := head
+	for node != nil {
+		size += 1
+		node = node.Next
+	}
+	return size
+}
