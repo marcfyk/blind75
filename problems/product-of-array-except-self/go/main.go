2b8b5995e15849b6c01662ae91c617eb540741f5
Marc Fong <marcjohnfong@gmail.com>
2024-03-03 17:42:42 +0800

    Update solutions for product-of-array-except-self

---
+++ problems/product-of-array-except-self/go/main.go
@@ -0,0 +1,17 @@
+package main
+
+func productExceptSelf(nums []int) []int {
+	products := make([]int, len(nums))
+
+	prefixAcc := 1
+	for i, n := range nums {
+		products[i] = prefixAcc
+		prefixAcc *= n
+	}
+
+	postfixAcc := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		products[i] *= postfixAcc
+		postfixAcc *= nums[i]
+	}
+}
